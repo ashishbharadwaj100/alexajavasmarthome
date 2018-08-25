9322eeefccbc3940b785a6fa76dabfa213edbfee
Abinash <Abinash@DESKTOP-5F519GG>
2018-08-25 14:49:32 +0530

    second commit

--- src/main/java/com/controlmyspa/voice2/alexa/config/AlexaConfig.java
+++ src/main/java/com/controlmyspa/voice2/alexa/config/AlexaConfig.java
@@ -1,8 +1,37 @@
 package com.controlmyspa.voice2.alexa.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.amazon.speech.json.SpeechletRequestModule;
+import com.amazon.speech.speechlet.servlet.SpeechletServlet;
+import com.controlmyspa.voice2.alexa.handlers.HandlerSpeechlet;
+import com.fasterxml.jackson.databind.Module;
+
+
 
 @Configuration
 public class AlexaConfig {
 
+	@Autowired
+	private HandlerSpeechlet handlerSpeechlet;
+	
+	@Bean
+	public ServletRegistrationBean registerSpeechletServlet() {
+		
+		SpeechletServlet speechletServlet = new SpeechletServlet();
+		speechletServlet.setSpeechlet(handlerSpeechlet);
+		
+		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(speechletServlet, "/alexa");
+		return servletRegistrationBean;
+	}
+	
+	@Bean
+	Module speechletRequestModule() {
+	    return new SpeechletRequestModule();
+	}
+
 }
+
